d3c0d0b57ad4b7521b53073bab2edd051aeb2c3f
105hua <joshuajtudball2003@gmail.com>
2024-09-15 01:54:54 +0100

    Created plugin with minecraft wizard.

---
+++ src/main/java/com/nearvanilla/iceCream/IceCream.java
@@ -0,0 +1,17 @@
+package com.nearvanilla.iceCream;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class IceCream extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}

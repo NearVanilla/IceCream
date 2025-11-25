4a7fc765ef2cd3c9bb9c9d99dc640444ac00a25c
Josh T <51213816+105hua@users.noreply.github.com>
2025-11-25 20:04:50 +0000

    Update dependencies. (#20)
    * Add Staff Mode module
    
    * Get Player PDC directly from player object.
    
    * Remove the need for initKeys() function.
    
    * Remove the need for serializing the players location as a byte array.
    
    * Code quality checks.
    
    * Re-added initKeys() function and updated dependencies as well as MC version.
    
    * Remove unnecessary comment.
    
    ---------
    
    Co-authored-by: Kid <29415728+Kiddooo@users.noreply.github.com>

--- src/main/java/com/nearvanilla/iceCream/modules/staffMode/StaffModeModule.java
+++ src/main/java/com/nearvanilla/iceCream/modules/staffMode/StaffModeModule.java
@@ -18,24 +18,30 @@ import org.bukkit.NamespacedKey;
  */
 public class StaffModeModule implements Module {
   protected boolean isEnabled = false;
-  public static NamespacedKey STAFF_MODE_TOGGLE_KEY =
-      new NamespacedKey(IceCream.instance, "staff_mode.enabled");
-  public static NamespacedKey STAFF_MODE_INVENTORY_KEY =
-      new NamespacedKey(IceCream.instance, "staff_mode.inventory");
-  public static NamespacedKey STAFF_MODE_LOCATION_WORLD =
-      new NamespacedKey(IceCream.instance, "staff_mode.location.world");
-  public static NamespacedKey STAFF_MODE_LOCATION_X =
-      new NamespacedKey(IceCream.instance, "staff_mode.location.x");
-  public static NamespacedKey STAFF_MODE_LOCATION_Y =
-      new NamespacedKey(IceCream.instance, "staff_mode.location.y");
-  public static NamespacedKey STAFF_MODE_LOCATION_Z =
-      new NamespacedKey(IceCream.instance, "staff_mode.location.z");
-  public static NamespacedKey STAFF_MODE_LOCATION_YAW =
-      new NamespacedKey(IceCream.instance, "staff_mode.location.yaw");
-  public static NamespacedKey STAFF_MODE_LOCATION_PITCH =
-      new NamespacedKey(IceCream.instance, "staff_mode.location.pitch");
-  public static NamespacedKey STAFF_MODE_CONFIRM_KEY =
-      new NamespacedKey(IceCream.instance, "staff_mode.confirm_disable");
+  public static NamespacedKey STAFF_MODE_TOGGLE_KEY;
+  public static NamespacedKey STAFF_MODE_INVENTORY_KEY;
+  public static NamespacedKey STAFF_MODE_LOCATION_WORLD;
+  public static NamespacedKey STAFF_MODE_LOCATION_X;
+  public static NamespacedKey STAFF_MODE_LOCATION_Y;
+  public static NamespacedKey STAFF_MODE_LOCATION_Z;
+  public static NamespacedKey STAFF_MODE_LOCATION_YAW;
+  public static NamespacedKey STAFF_MODE_LOCATION_PITCH;
+  public static NamespacedKey STAFF_MODE_CONFIRM_KEY;
+
+  private static void initKeys() {
+    if (STAFF_MODE_TOGGLE_KEY != null) {
+      return;
+    }
+    STAFF_MODE_TOGGLE_KEY = new NamespacedKey(IceCream.instance, "staff_mode.enabled");
+    STAFF_MODE_INVENTORY_KEY = new NamespacedKey(IceCream.instance, "staff_mode.inventory");
+    STAFF_MODE_LOCATION_WORLD = new NamespacedKey(IceCream.instance, "staff_mode.location.world");
+    STAFF_MODE_LOCATION_X = new NamespacedKey(IceCream.instance, "staff_mode.location.x");
+    STAFF_MODE_LOCATION_Y = new NamespacedKey(IceCream.instance, "staff_mode.location.y");
+    STAFF_MODE_LOCATION_Z = new NamespacedKey(IceCream.instance, "staff_mode.location.z");
+    STAFF_MODE_LOCATION_YAW = new NamespacedKey(IceCream.instance, "staff_mode.location.yaw");
+    STAFF_MODE_LOCATION_PITCH = new NamespacedKey(IceCream.instance, "staff_mode.location.pitch");
+    STAFF_MODE_CONFIRM_KEY = new NamespacedKey(IceCream.instance, "staff_mode.confirm_disable");
+  }
 
   @Override
   public boolean shouldEnable() {
@@ -62,6 +68,7 @@ public class StaffModeModule implements Module {
 
   @Override
   public void register() {
+    initKeys();
     if (shouldEnable()) {
       try {
         registerCommands();
